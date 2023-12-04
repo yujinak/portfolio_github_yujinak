00a55023490b2b0e00bd48a1a068da4b3022f208
Yuji <g.yujinak@gmail.com>
2023-12-04 14:39:36 -0300

    Configuring development  environment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './Teste'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <>
+      <Teste />
+      <EstiloGlobal />
+    </>
+  )
+}
+
+export default App
